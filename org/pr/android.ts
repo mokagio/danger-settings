cd21052d06dd2356bc3a0c37845edcd0bacb2d5d
Giovanni Lodi <giovanni.lodi42@gmail.com>
2020-02-14 11:30:50 +1100

    Make org/pr/android.ts "sync"

--- org/pr/android.ts
+++ org/pr/android.ts
@@ -1,14 +1,10 @@
 import {warn, danger} from "danger";
 
-export default async () => {
+// If changes were made to the release notes, there must also be changes to the PlayStoreStrings file.
+const modifiedFiles = danger.git.modified_files;
+const hasModifiedReleaseNotes = modifiedFiles.some(f => f.endsWith("metadata/release_notes.txt"));
+const hasModifiedPlayStoreStrings = modifiedFiles.some(f => f.includes("metadata/PlayStoreStrings.po"));
 
-    // If changes were made to the release notes, there must also be changes to the PlayStoreStrings file.
-    const modifiedFiles = danger.git.modified_files;
-    const hasModifiedReleaseNotes = modifiedFiles.some(f => f.endsWith("metadata/release_notes.txt"));
-    const hasModifiedPlayStoreStrings = modifiedFiles.some(f => f.includes("metadata/PlayStoreStrings.po"));
-
-    if (hasModifiedReleaseNotes && !hasModifiedPlayStoreStrings) {
-        warn("The PlayStoreStrings.po file must be updated any time changes are made to release notes");
-    }
-
-};
+if (hasModifiedReleaseNotes && !hasModifiedPlayStoreStrings) {
+  warn("The PlayStoreStrings.po file must be updated any time changes are made to release notes");
+}
