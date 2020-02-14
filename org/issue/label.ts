5312523b824c32e1e46b8072570bb358184f40aa
Giovanni Lodi <giovanni.lodi42@gmail.com>
2020-02-14 11:30:05 +1100

    Make org/issue/label.ts "sync"

--- org/issue/label.ts
+++ org/issue/label.ts
@@ -1,20 +1,18 @@
 import {fail, danger} from "danger";
 
-export default async () => {
-    const githubLabels = danger.github.issue.labels;
+const githubLabels = danger.github.issue.labels;
 
-    // An issue should have at least one type label and at least one feature label
+// An issue should have at least one type label and at least one feature label
 
-    // Type labels start with [Something]
-    const typeRegex = RegExp('^\\[.+\\].*');
-    const typeLabel = githubLabels.some(label => typeRegex.test(label.name));
-    if (!typeLabel) {
-        fail("Please add a type label to this issue. e.g. '[Type] Enhancement'");
-    }
+// Type labels start with [Something]
+const typeRegex = RegExp('^\\[.+\\].*');
+const typeLabel = githubLabels.some(label => typeRegex.test(label.name));
+if (!typeLabel) {
+  fail("Please add a type label to this issue. e.g. '[Type] Enhancement'");
+}
 
-    // Feature labels don't start with [Something]
-    const featureLabel = githubLabels.some(label => !typeRegex.test(label.name));
-    if (!featureLabel) {
-        fail("Please add a feature label to this issue. e.g. 'Stats'");
-    }
-};
+// Feature labels don't start with [Something]
+const featureLabel = githubLabels.some(label => !typeRegex.test(label.name));
+if (!featureLabel) {
+  fail("Please add a feature label to this issue. e.g. 'Stats'");
+}
